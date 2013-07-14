de642eeff2ed31070ce84fbe03b1108de6c207b2
Jan Horava <czechinthemail@gmail.com>
2013-07-13 22:02:58 -0700

    improved formatting

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A JavaScript game in which you must click the randomly generated canvas shapes as quickly as possible. You can change how many shapes appear, the restart key, what the time limit is, the audio volume, and if the canvas clears or not. Settings are saved in localStorage.
+A web-based game in which you must click the randomly generated canvas shapes as quickly as possible. Users can change how many shapes appear, the restart key, what the time limit is, the audio volume, and if the canvas clears or not. Settings are saved in localStorage.
--- js/speedshape.js
+++ js/speedshape.js
@@ -9,7 +9,7 @@ function decisecond(){
 }
 
 function draw(){
-    if(settings[4]){/* Clear? */
+    if(settings[4]){// Clear?
         buffer.clearRect(
             0,
             0,
@@ -51,7 +51,7 @@ function draw(){
         55
     );
 
-    if(settings[4]){/* Clear? */
+    if(settings[4]){// Clear?
         canvas.clearRect(
             0,
             0,
@@ -71,14 +71,14 @@ function get(i){
 }
 
 function play_audio(i){
-    if(settings[0] > 0){/* Audio Volume */
+    if(settings[0] > 0){// Audio Volume
         get(i).currentTime = 0;
         get(i).play();
     }
 }
 
 function randomize_shapes(){
-    if(settings[1] > 0){/* Number of Reds */
+    if(settings[1] > 0){// Number of Reds
         reds = [];
         i = settings[1] - 1;
         do{
@@ -129,7 +129,7 @@ function resize(){
 function save(){
     if(get('restart-key').value === 'H'){
         ls.removeItem('speedshape-3');
-        settings[3] = 'H';/* Restart Key? */
+        settings[3] = 'H';// Restart Key?
 
     }else{
         settings[3] = get('restart-key').value;
@@ -164,7 +164,7 @@ function save(){
         }
     }while(i--);
 
-    settings[4] = get('clear').checked;/* Clear? */
+    settings[4] = get('clear').checked;// Clear?
     if(settings[4]){
         ls.removeItem('speedshape-4');
 
@@ -180,7 +180,7 @@ function setmode(newmode, newgame){
     clearInterval(interval);
     mode = newmode;
 
-    /* new game mode */
+    // new game mode
     if(mode > 0){
         if(newgame){
             save();
@@ -201,14 +201,14 @@ function setmode(newmode, newgame){
 
         interval = setInterval('decisecond()', 100);
 
-    /* main menu mode */
+    // main menu mode
     }else{
         buffer = 0;
         canvas = 0;
 
-        get('page').innerHTML = '<div style="border-right:8px solid #222;display:inline-block;text-align:left;vertical-align:top"><div class=c><b>Speedshape</b></div><hr><div class=c><a onclick="setmode(1, 1)">Start New Game</a></div><hr><div class=c><input id=reds size=1 type=text value='
-            + settings[1] + '>Red<br><input id=time-limit size=1 type=text value='
-            + settings[2] + '>Time Limit</div></div><div style=display:inline-block;text-align:left><div class=c><input disabled size=3 style=border:0 type=text value=ESC>Main Menu<br><input id=restart-key maxlength=1 size=3 type=text value='
+        get('page').innerHTML = '<div style="border-right:8px solid #222;display:inline-block;text-align:left;vertical-align:top"><div class=c><b>Speedshape</b></div><hr><div class=c><a onclick="setmode(1, 1)">Start New Game</a></div><hr><div class=c><input id=reds size=1 value='
+            + settings[1] + '>Red<br><input id=time-limit size=1 value='
+            + settings[2] + '>Time Limit</div></div><div style=display:inline-block;text-align:left><div class=c><input disabled size=3 style=border:0 value=ESC>Main Menu<br><input id=restart-key maxlength=1 size=3 value='
             + settings[3] + '>Restart</div><hr><div class=c><input id=audio-volume max=1 min=0 step=.01 type=range value='
             + settings[0] + '>Audio<br><label><input '
             + (settings[4] ? 'checked ' : '') + 'id=clear type=checkbox>Clear</label><br><a onclick="if(confirm(\'Reset settings?\')){get(\'clear\').checked=get(\'audio-volume\').value=1;get(\'restart-key\').value=\'H\';get(\'reds\').value=10;get(\'time-limit\').value=30;save();setmode(0,1)}">Reset Settings</a></div></div>';
@@ -228,11 +228,11 @@ var mouse_y = 0;
 var reds = [];
 var score = 0;
 var settings = [
-    ls.getItem('speedshape-0') === null ?   1 : parseFloat(ls.getItem('speedshape-0')),/* Audio Volume */
-    ls.getItem('speedshape-1') === null ?  10 : parseInt(ls.getItem('speedshape-1')),/* Number of Reds */
-    ls.getItem('speedshape-2') === null ?  30 : parseInt(ls.getItem('speedshape-2')),/* Time Limit */
-    ls.getItem('speedshape-3') === null ? 'H' : ls.getItem('speedshape-3'),/* Reset Key */
-    ls.getItem('speedshape-4') === null/* Clear? */
+    ls.getItem('speedshape-0') === null ?   1 : parseFloat(ls.getItem('speedshape-0')),// Audio Volume
+    ls.getItem('speedshape-1') === null ?  10 : parseInt(ls.getItem('speedshape-1')),// Number of Reds
+    ls.getItem('speedshape-2') === null ?  30 : parseInt(ls.getItem('speedshape-2')),// Time Limit
+    ls.getItem('speedshape-3') === null ? 'H' : ls.getItem('speedshape-3'),// Reset Key
+    ls.getItem('speedshape-4') === null// Clear?
 ];
 var time = 0;
 var white = [];
@@ -245,10 +245,10 @@ window.onkeydown = function(e){
         i = window.event ? event : e;
         i = i.charCode ? i.charCode : i.keyCode;
 
-        if(String.fromCharCode(i) === settings[3]){/* Reset Key? */
+        if(String.fromCharCode(i) === settings[3]){// Reset Key?
             setmode(1, 0);
 
-        }else if(i === 27){/* ESC */
+        }else if(i === 27){// ESC
             setmode(0, 0);
         }
     }
