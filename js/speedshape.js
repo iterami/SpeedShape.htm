8bd95f71d7aa47d2c41bb0db4e64735b403e7ef8
Jan Horava <czechinthemail@gmail.com>
2014-03-15 14:17:33 -0700

    removed ls variable, improved formatting

--- js/speedshape.js
+++ js/speedshape.js
@@ -11,10 +11,10 @@ function decisecond(){
 function draw(){
     if(settings[4]){// Clear?
         buffer.clearRect(
-            0,
-            0,
-            width,
-            height
+          0,
+          0,
+          width,
+          height
         );
     }
 
@@ -23,46 +23,46 @@ function draw(){
         buffer.fillStyle = '#f00';
         do{
             buffer.fillRect(
-                reds[i][0],
-                reds[i][1],
-                reds[i][2],
-                reds[i][3]
+              reds[i][0],
+              reds[i][1],
+              reds[i][2],
+              reds[i][3]
             );
         }while(i--);
     }
 
     buffer.fillStyle = '#fff';
     buffer.fillRect(
-        white[0],
-        white[1],
-        white[2],
-        white[3]
+      white[0],
+      white[1],
+      white[2],
+      white[3]
     );
 
     buffer.font = '23pt sans-serif';
     buffer.fillText(
-        'Time: ' + time + '/' + settings[2],
-        5,
-        25
+      'Time: ' + time + '/' + settings[2],
+      5,
+      25
     );
     buffer.fillText(
-        'Score: ' + score,
-        5,
-        55
+      'Score: ' + score,
+      5,
+      55
     );
 
     if(settings[4]){// Clear?
         canvas.clearRect(
-            0,
-            0,
-            width,
-            height
+          0,
+          0,
+          width,
+          height
         );
     }
     canvas.drawImage(
-        document.getElementById('buffer'),
-        0,
-        0
+      document.getElementById('buffer'),
+      0,
+      0
     );
 }
 
@@ -79,19 +79,19 @@ function randomize_shapes(){
         i = settings[1] - 1;
         do{
             reds.push([
-                random_number(width) - 21,
-                random_number(height) - 21,
-                random_number(200) + 42,
-                random_number(200) + 42
+              random_number(width) - 21,
+              random_number(height) - 21,
+              random_number(200) + 42,
+              random_number(200) + 42
             ]);
         }while(i--);
     }
 
     white = [
-        random_number(width) - 9,
-        random_number(height) - 9,
-        random_number(99) + 20,
-        random_number(99) + 20
+      random_number(width) - 9,
+      random_number(height) - 9,
+      random_number(99) + 20,
+      random_number(99) + 20
     ];
 
     if(time <= 0){
@@ -135,50 +135,52 @@ function resize(){
 
 function save(){
     if(document.getElementById('restart-key').value === 'H'){
-        ls.removeItem('speedshape-3');
+        window.localStorage.removeItem('speedshape-3');
         settings[3] = 'H';// Restart Key?
 
     }else{
         settings[3] = document.getElementById('restart-key').value;
-        ls.setItem(
-            'speedshape-3',
-            settings[3]
+        window.localStorage.setItem(
+          'speedshape-3',
+          settings[3]
         );
     }
 
     i = 2;
     do{
         j = [
-            'audio-volume',
-            'reds',
-            'time-limit'
+          'audio-volume',
+          'reds',
+          'time-limit'
         ][i];
-        if(isNaN(document.getElementById(j).value) || document.getElementById(j).value === [1, 10, 30][i] || document.getElementById(j).value < [0, 0, 1][i]){
-            ls.removeItem('speedshape-' + i);
+        if(isNaN(document.getElementById(j).value)
+          || document.getElementById(j).value === [1, 10, 30][i]
+          || document.getElementById(j).value < [0, 0, 1][i]){
+            window.localStorage.removeItem('speedshape-' + i);
             settings[i] = [
-                1,
-                10,
-                30
+              1,
+              10,
+              30
             ][i];
             document.getElementById(j).value = settings[i];
 
         }else{
             settings[i] = parseFloat(document.getElementById(j).value);
-            ls.setItem(
-                'speedshape-' + i,
-                settings[i]
+            window.localStorage.setItem(
+              'speedshape-' + i,
+              settings[i]
             );
         }
     }while(i--);
 
     settings[4] = document.getElementById('clear').checked;// Clear?
     if(settings[4]){
-        ls.removeItem('speedshape-4');
+        window.localStorage.removeItem('speedshape-4');
 
     }else{
-        ls.setItem(
-            'speedshape-4',
-            0
+        window.localStorage.setItem(
+          'speedshape-4',
+          0
         );
     }
 }
@@ -214,11 +216,11 @@ function setmode(newmode, newgame){
         canvas = 0;
 
         document.getElementById('page').innerHTML = '<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><b>Speedshape</b></div><hr><div class=c><ul><li><a onclick="setmode(1, 1)">Start New Game</a></ul></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c><input disabled style=border:0 value=ESC>Main Menu<br><input id=restart-key maxlength=1 value='
-            + settings[3] + '>Restart</div><hr><div class=c><input id=audio-volume max=1 min=0 step=.01 type=range value='
-            + settings[0] + '>Audio<br><label><input '
-            + (settings[4] ? 'checked ' : '') + 'id=clear type=checkbox>Clear</label><br><input id=reds value='
-            + settings[1] + '>Red<br><input id=time-limit value='
-            + settings[2] + '>Time Limit<br><a onclick=reset()>Reset Settings</a></div></div>';
+          + settings[3] + '>Restart</div><hr><div class=c><input id=audio-volume max=1 min=0 step=.01 type=range value='
+          + settings[0] + '>Audio<br><label><input '
+          + (settings[4] ? 'checked ' : '') + 'id=clear type=checkbox>Clear</label><br><input id=reds value='
+          + settings[1] + '>Red<br><input id=time-limit value='
+          + settings[2] + '>Time Limit<br><a onclick=reset()>Reset Settings</a></div></div>';
     }
 }
 
@@ -228,18 +230,26 @@ var height = 0;
 var i = 0;
 var interval = 0;
 var j = 0;
-var ls = window.localStorage;
 var mode = 0;
 var mouse_x = 0;
 var mouse_y = 0;
 var reds = [];
 var score = 0;
 var settings = [
-    ls.getItem('speedshape-0') === null ?   1 : parseFloat(ls.getItem('speedshape-0')),// Audio Volume
-    ls.getItem('speedshape-1') === null ?  10 : parseInt(ls.getItem('speedshape-1')),// Number of Reds
-    ls.getItem('speedshape-2') === null ?  30 : parseInt(ls.getItem('speedshape-2')),// Time Limit
-    ls.getItem('speedshape-3') === null ? 'H' : ls.getItem('speedshape-3'),// Reset Key
-    ls.getItem('speedshape-4') === null// Clear?
+  window.localStorage.getItem('speedshape-0') === null
+    ? 1
+    : parseFloat(window.localStorage.getItem('speedshape-0')),// Audio Volume
+  window.localStorage.getItem('speedshape-1') === null
+    ? 10
+    : parseInt(window.localStorage.getItem('speedshape-1')),// Number of Reds
+  window.localStorage.getItem('speedshape-2') === null
+    ? 30
+    : parseInt(window.localStorage.getItem('speedshape-2')),// Time Limit
+  window.localStorage.getItem('speedshape-3') === null
+    ?
+    'H'
+    : window.localStorage.getItem('speedshape-3'),// Reset Key
+  window.localStorage.getItem('speedshape-4') === null// Clear?
 ];
 var time = 0;
 var white = [];
@@ -269,9 +279,9 @@ window.onmousedown = function(e){
         mouse_y = e.pageY;
 
         if(mouse_x > white[0]
-         && mouse_x < white[0] + white[2]
-         && mouse_y > white[1]
-         && mouse_y < white[1] + white[3]){
+          && mouse_x < white[0] + white[2]
+          && mouse_y > white[1]
+          && mouse_y < white[1] + white[3]){
             score += 1;
             randomize_shapes();
 
@@ -280,9 +290,9 @@ window.onmousedown = function(e){
             if(i >= 0){
                 do{
                     if(mouse_x > reds[i][0]
-                     && mouse_x < reds[i][0] + reds[i][2]
-                     && mouse_y > reds[i][1]
-                     && mouse_y < reds[i][1] + reds[i][3]){
+                      && mouse_x < reds[i][0] + reds[i][2]
+                      && mouse_y > reds[i][1]
+                      && mouse_y < reds[i][1] + reds[i][3]){
                         score -= 1;
                         randomize_shapes();
                         break;
