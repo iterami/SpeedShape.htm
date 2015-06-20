8a0a716a646b1edf7e99f6a97ee917b236a206ac
Jan Horava <czechinthemail@gmail.com>
2015-06-20 09:37:15 -0700

    fixed handling 0 settings

--- js/speedshape.js
+++ js/speedshape.js
@@ -242,11 +242,17 @@ var mouse_x = 0;
 var mouse_y = 0;
 var score = 0;
 var settings = {
-  'audio-volume': parseFloat(window.localStorage.getItem('SpeedShape.htm-audio-volume')) || 1,
-  'reds': parseInt(window.localStorage.getItem('SpeedShape.htm-reds')) || 10,
+  'audio-volume': window.localStorage.getItem('SpeedShape.htm-audio-volume') != null
+    ? parseFloat(window.localStorage.getItem('SpeedShape.htm-audio-volume'))
+    : 1,
+  'reds': window.localStorage.getItem('SpeedShape.htm-reds') != null
+    ? parseInt(window.localStorage.getItem('SpeedShape.htm-reds'))
+    : 10,
   'restart-key': window.localStorage.getItem('SpeedShape.htm-restart-key') || 'H',
   'time-limit': parseInt(window.localStorage.getItem('SpeedShape.htm-time-limit')) || 30,
-  'whites': parseInt(window.localStorage.getItem('SpeedShape.htm-whites')) || 1,
+  'whites': window.localStorage.getItem('SpeedShape.htm-whites') != null
+    ? parseInt(window.localStorage.getItem('SpeedShape.htm-whites'))
+    : 1,
 };
 var shapes = [];
 var time = 0;
