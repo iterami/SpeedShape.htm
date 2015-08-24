e6d807a04f79159c4ea51968fd42b33f6fae7557
Jan Horava <czechinthemail@gmail.com>
2015-08-23 17:52:28 -0700

    moved disabled input css to css file

--- js/speedshape.js
+++ js/speedshape.js
@@ -236,7 +236,7 @@ function setmode(newmode, newgame){
     buffer = 0;
     canvas = 0;
 
-    document.getElementById('page').innerHTML = '<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><a onclick="setmode(1, true)">Start New Game</a></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c><input disabled style=border:0 value=ESC>Main Menu<br><input id=restart-key maxlength=1 value='
+    document.getElementById('page').innerHTML = '<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><a onclick="setmode(1, true)">Start New Game</a></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c><input disabled value=ESC>Main Menu<br><input id=restart-key maxlength=1 value='
       + settings['restart-key'] + '>Restart</div><hr><div class=c><input id=audio-volume max=1 min=0 step=.01 type=range value='
       + settings['audio-volume'] + '>Audio<br><input id=reds value='
       + settings['reds'] + '>Red<br><input id=time-limit value='
