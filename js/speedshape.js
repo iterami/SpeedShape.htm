41a98b72a6d941eadcb2078cc8096cbdbad76bb4
honzi <czechinthemail@gmail.com>
2016-04-17 18:34:41 -0700

    temporarily removed play_audio()

--- js/speedshape.js
+++ js/speedshape.js
@@ -57,15 +57,6 @@ function logic(){
     }
 }
 
-function play_audio(id){
-    if(settings['audio-volume'] <= 0){
-        return;
-    }
-
-    document.getElementById(id).currentTime = 0;
-    document.getElementById(id).play();
-}
-
 function randomize_shapes(){
     shapes.length = 0;
 
