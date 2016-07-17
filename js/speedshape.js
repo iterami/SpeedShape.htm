b45b2c52ff1ee26930b9b63114c52ebe95ea63b7
honzi <czechinthemail@gmail.com>
2016-07-17 12:26:20 -0700

    switched to common random.js usage

--- js/speedshape.js
+++ js/speedshape.js
@@ -44,11 +44,11 @@ function randomize_shapes(){
         do{
             shapes.push({
               'color': '#f00',
-              'height': Math.floor(Math.random() * 200) + 42,
+              'height': random_integer(200) + 42,
               'score': -1,
-              'width': Math.floor(Math.random() * 200) + 42,
-              'x': Math.floor(Math.random() * canvas_width) - 21,
-              'y': Math.floor(Math.random() * canvas_height) - 21,
+              'width': random_integer(200) + 42,
+              'x': random_integer(canvas_width) - 21,
+              'y': random_integer(canvas_height) - 21,
             });
         }while(loop_counter--);
     }
@@ -57,11 +57,11 @@ function randomize_shapes(){
         do{
             shapes.push({
               'color': '#fff',
-              'height': Math.floor(Math.random() * 99) + 20,
+              'height': random_integer(99) + 20,
               'score': 1,
-              'width': Math.floor(Math.random() * 99) + 20,
-              'x': Math.floor(Math.random() * canvas_width) - 9,
-              'y': Math.floor(Math.random() * canvas_height) - 9,
+              'width': random_integer(99) + 20,
+              'x': random_integer(canvas_width) - 9,
+              'y': random_integer(canvas_height) - 9,
             });
         }while(loop_counter--);
     }
