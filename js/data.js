4922cf49e594d08cce2b2b4196e60feac0532303
honzi <czechinthemail@gmail.com>
2017-07-13 01:56:13 -0700

    converted to entities

--- js/data.js
+++ js/data.js
@@ -1,8 +1,6 @@
 'use strict';
 
 function load_data(id){
-    shapes.length = 0;
-    shapes_length = 0;
     randomize_shapes();
     score = 0;
     time = core_storage_data['time-limit'];
--- js/speedshape.js
+++ js/speedshape.js
@@ -2,13 +2,13 @@
 
 function draw_logic(){
     // Draw shapes.
-    for(var shape in shapes){
-        canvas_buffer.fillStyle = shapes[shape]['color'];
+    for(var entity in core_entities){
+        canvas_buffer.fillStyle = core_entities[entity]['color'];
         canvas_buffer.fillRect(
-          shapes[shape]['x'],
-          shapes[shape]['y'],
-          shapes[shape]['width'],
-          shapes[shape]['height']
+          core_entities[entity]['x'],
+          core_entities[entity]['y'],
+          core_entities[entity]['width'],
+          core_entities[entity]['height']
         );
     }
 }
@@ -30,52 +30,54 @@ function logic(){
 }
 
 function randomize_shapes(){
-    shapes.length = 0;
+    core_entities = {};
 
-    if(core_storage_data['red'] > 0){
-        var loop_counter = core_storage_data['red'] - 1;
+    if(core_storage_data['negative'] > 0){
+        var loop_counter = core_storage_data['negative'] - 1;
         do{
-            shapes.push({
-              'color': core_storage_data['color-negative'],
-              'height': core_random_integer({
-                'max': 200,
-              }) + 42,
-              'score': -1,
-              'width': core_random_integer({
-                'max': 200,
-              }) + 42,
-              'x': core_random_integer({
-                'max': canvas_width,
-              }) - 21,
-              'y': core_random_integer({
-                'max': canvas_height,
-              }) - 21,
+            core_entity_create({
+              'properties': {
+                'color': core_storage_data['color-negative'],
+                'height': core_random_integer({
+                  'max': 200,
+                }) + 42,
+                'score': -1,
+                'width': core_random_integer({
+                  'max': 200,
+                }) + 42,
+                'x': core_random_integer({
+                  'max': canvas_width,
+                }) - 21,
+                'y': core_random_integer({
+                  'max': canvas_height,
+                }) - 21,
+              },
             });
         }while(loop_counter--);
     }
-    if(core_storage_data['white'] > 0){
-        var loop_counter = core_storage_data['white'] - 1;
+    if(core_storage_data['positive'] > 0){
+        var loop_counter = core_storage_data['positive'] - 1;
         do{
-            shapes.push({
-              'color': core_storage_data['color-positive'],
-              'height': core_random_integer({
-                'max': 99,
-              }) + 20,
-              'score': 1,
-              'width': core_random_integer({
-                'max': 99,
-              }) + 20,
-              'x': core_random_integer({
-                'max': canvas_width,
-              }) - 9,
-              'y': core_random_integer({
-                'max': canvas_height,
-              }) - 9,
+            core_entity_create({
+              'properties': {
+                'color': core_storage_data['color-positive'],
+                'height': core_random_integer({
+                  'max': 99,
+                }) + 20,
+                'score': 1,
+                'width': core_random_integer({
+                  'max': 99,
+                }) + 20,
+                'x': core_random_integer({
+                  'max': canvas_width,
+                }) - 9,
+                'y': core_random_integer({
+                  'max': canvas_height,
+                }) - 9,
+              },
             });
         }while(loop_counter--);
     }
-
-    shapes_length = shapes.length;
 
     if(time <= 0){
         canvas_draw();
@@ -104,34 +106,43 @@ function repo_init(){
                   return;
               }
 
-              for(var shape in shapes){
-                  shape = shapes_length - shape - 1;
+              var dscore = 0;
 
-                  if(core_mouse['x'] <= shapes[shape]['x']
-                    || core_mouse['x'] >= shapes[shape]['x'] + shapes[shape]['width']
-                    || core_mouse['y'] <= shapes[shape]['y']
-                    || core_mouse['y'] >= shapes[shape]['y'] + shapes[shape]['height']){
+              for(var entity in core_entities){
+                  if(core_mouse['x'] <= core_entities[entity]['x']
+                    || core_mouse['x'] >= core_entities[entity]['x'] + core_entities[entity]['width']
+                    || core_mouse['y'] <= core_entities[entity]['y']
+                    || core_mouse['y'] >= core_entities[entity]['y'] + core_entities[entity]['height']){
                       continue;
                   }
+
+                  if(dscore <= 0){
+                      dscore = core_entities[entity]['score'];
+
+                  }else{
+                      break;
+                  }
+              }
+
+              if(dscore !== 0){
+                  score += dscore;
 
                   core_audio_start({
                     'id': 'boop',
                   });
 
-                  score += shapes[shape]['score'];
                   randomize_shapes();
-                  break;
               }
           },
         },
       },
       'storage': {
         'ms-per-frame': 100,
-        'red': 10,
+        'negative': 10,
+        'positive': 1,
         'time-limit': 30,
-        'white': 1,
       },
-      'storage-menu': '<table><tr><td><input id=ms-per-frame><td>ms/Frame<tr><td><input id=red><td>Red<tr><td><input id=time-limit><td>Time Limit<tr><td><input id=white><td>White</table>',
+      'storage-menu': '<table><tr><td><input id=ms-per-frame><td>ms/Frame<tr><td><input id=negative><td># of Negative<tr><td><input id=time-limit><td>Time Limit<tr><td><input id=positive><td># of Positive</table>',
       'title': 'SpeedShape.htm',
       'ui': '<input id=ui-score>Score<br><input id=ui-time>Time',
     });
@@ -148,7 +159,5 @@ function resize_logic(){
 }
 
 var score = 0;
-var shapes = [];
-var shapes_length = 0;
 var time = 0;
 var time_display = '';
