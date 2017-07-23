7a9d9bc40205a82b06b4ac268e101bda4c9a7a50
honzi <czechinthemail@gmail.com>
2017-07-23 13:08:13 -0700

    moved non-common functions into data.js

--- js/speedshape.js
+++ js/speedshape.js
@@ -29,61 +29,6 @@ function logic(){
     });
 }
 
-function randomize_shapes(){
-    core_entity_remove_all();
-
-    if(core_storage_data['negative'] > 0){
-        var loop_counter = core_storage_data['negative'] - 1;
-        do{
-            core_entity_create({
-              'properties': {
-                'color': core_storage_data['color-negative'],
-                'height': core_random_integer({
-                  'max': 200,
-                }) + 42,
-                'score': -1,
-                'width': core_random_integer({
-                  'max': 200,
-                }) + 42,
-                'x': core_random_integer({
-                  'max': canvas_width,
-                }) - 21,
-                'y': core_random_integer({
-                  'max': canvas_height,
-                }) - 21,
-              },
-            });
-        }while(loop_counter--);
-    }
-    if(core_storage_data['positive'] > 0){
-        var loop_counter = core_storage_data['positive'] - 1;
-        do{
-            core_entity_create({
-              'properties': {
-                'color': core_storage_data['color-positive'],
-                'height': core_random_integer({
-                  'max': 99,
-                }) + 20,
-                'score': 1,
-                'width': core_random_integer({
-                  'max': 99,
-                }) + 20,
-                'x': core_random_integer({
-                  'max': canvas_width,
-                }) - 9,
-                'y': core_random_integer({
-                  'max': canvas_height,
-                }) - 9,
-              },
-            });
-        }while(loop_counter--);
-    }
-
-    if(time <= 0){
-        canvas_draw();
-    }
-}
-
 function repo_init(){
     core_repo_init({
       'audios': {
