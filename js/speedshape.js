1b40deca1f43d92480bda6364ca5e97112383c94
honzi <czechinthemail@gmail.com>
2017-08-18 13:07:35 -0700

    replaced inline js with info-events usage

--- js/speedshape.js
+++ js/speedshape.js
@@ -45,7 +45,17 @@ function repo_init(){
           'duration': .1,
         },
       },
-      'info': '<input onclick=canvas_setmode({newgame:true}) type=button value="Start New Game">',
+      'info': '<input id=start type=button value="Start New Game">',
+      'info-events': {
+        'start': {
+          'todo': function(){
+              canvas_setmode({
+                'newgame': true,
+              });
+          },
+          'type': 'onclick',
+        },
+      },
       'keybinds': {
         72: {
           'todo': canvas_setmode,
