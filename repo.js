e14c153ade05967a81f6876d9c788036a129c679
Jan Hořava <czechinthemail@gmail.com>
2025-06-14 03:06:09 -0700

    simplified property usage

--- repo.js
+++ repo.js
@@ -1,18 +1,18 @@
 'use strict';
 
 function create_shape(type, loop_counter){
-    const max = core_storage_data[type + '-size-max'];
-    const bonus = core_storage_data[type + '-size-bonus'];
+    const max = core_storage_data[type + '_size_max'];
+    const bonus = core_storage_data[type + '_size_bonus'];
     entity_create({
-      'id': type + '-' + loop_counter,
+      'id': type + '_' + loop_counter,
       'properties': {
         'color': type === 'positive'
           ? '#206620'
           : '#663366',
         'height': core_random_integer(max) + bonus,
-        'width': core_random_integer(max,) + bonus,
-        'x': core_random_integer(canvas_properties['width']) - bonus / 2,
-        'y': core_random_integer(canvas_properties['height'] - 30) - bonus / 2 + 30,
+        'width': core_random_integer(max) + bonus,
+        'x': core_random_integer(canvas_properties.width) - bonus / 2,
+        'y': core_random_integer(canvas_properties.height - 30) - bonus / 2 + 30,
       },
     });
 }
@@ -20,16 +20,16 @@ function create_shape(type, loop_counter){
 function load_data(id){
     randomize_shapes();
     score = 0;
-    time = core_storage_data['time-limit'];
+    time = core_storage_data.time_limit;
 }
 
 function randomize_shapes(){
     entity_remove_all();
 
-    for(let i = 0; i < core_storage_data['negative-count']; i++){
+    for(let i = 0; i < core_storage_data.negative_count; i++){
         create_shape('negative', i);
     }
-    for(let i = 0; i < core_storage_data['positive-count']; i++){
+    for(let i = 0; i < core_storage_data.positive_count; i++){
         create_shape('positive', i);
     }
 
@@ -43,21 +43,21 @@ function repo_drawlogic(){
       ],
       'todo': function(entity){
           canvas_setproperties({
-            'fillStyle': entity['color'],
+            'fillStyle': entity.color,
           });
           canvas.fillRect(
-            entity['x'],
-            entity['y'],
-            entity['width'],
-            entity['height']
+            entity.x,
+            entity.y,
+            entity.width,
+            entity.height
           );
       },
     });
 }
 
 function repo_escape(){
-    if(!entity_entities['negative-0']
-      && !entity_entities['positive-0']
+    if(!entity_entities.negative_0
+      && !entity_entities.positive_0
       && !core_menu_open){
         start();
     }
@@ -85,7 +85,7 @@ function repo_init(){
               }
 
               const pixel = canvas.getImageData(
-                core_pointer['x'], core_pointer['y'],
+                core_pointer.x, core_pointer.y,
                 1, 1
               ).data[0];
               if(pixel === 0){
@@ -93,33 +93,33 @@ function repo_init(){
               }
 
               score += pixel === 102
-                ? core_storage_data['negative-score']
-                : core_storage_data['positive-score'];
+                ? core_storage_data.negative_score
+                : core_storage_data.positive_score;
               audio_start('boop');
               randomize_shapes();
           },
         },
       },
       'storage': {
-        'negative-count': 10,
-        'negative-score': -1,
-        'negative-size-bonus': 42,
-        'negative-size-max': 200,
-        'positive-count': 1,
-        'positive-score': 1,
-        'positive-size-bonus': 20,
-        'positive-size-max': 99,
-        'time-limit': 30,
+        'negative_count': 10,
+        'negative_score': -1,
+        'negative_size_bonus': 42,
+        'negative_size_max': 200,
+        'positive_count': 1,
+        'positive_score': 1,
+        'positive_size_bonus': 20,
+        'positive_size_max': 99,
+        'time_limit': 30,
       },
-      'storage-menu': '<table><tr><td><input class=mini id=negative-count min=0 step=1 type=number><td># of Negative'
-        + '<tr><td><input class=mini id=negative-score step=any type=number><td>Negative Score'
-        + '<tr><td><input class=mini id=negative-size-bonus step=any type=number><td>Negative Size Bonus'
-        + '<tr><td><input class=mini id=negative-size-max step=any type=number><td>Negative Size Max'
-        + '<tr><td><input class=mini id=positive-count min=0 step=1 type=number><td># of Positive'
-        + '<tr><td><input class=mini id=positive-score step=any type=number><td>Positive Score'
-        + '<tr><td><input class=mini id=positive-size-bonus step=any type=number><td>Positive Size Bonus'
-        + '<tr><td><input class=mini id=positive-size-max step=any type=number><td>Positive Size Max'
-        + '<tr><td><input class=mini id=time-limit step=any type=number><td>Time Limit</table>',
+      'storage-menu': '<table><tr><td><input class=mini id=negative_count min=0 step=1 type=number><td># of Negative'
+        + '<tr><td><input class=mini id=negative_score step=any type=number><td>Negative Score'
+        + '<tr><td><input class=mini id=negative_size_bonus step=any type=number><td>Negative Size Bonus'
+        + '<tr><td><input class=mini id=negative_size_max step=any type=number><td>Negative Size Max'
+        + '<tr><td><input class=mini id=positive_count min=0 step=1 type=number><td># of Positive'
+        + '<tr><td><input class=mini id=positive_score step=any type=number><td>Positive Score'
+        + '<tr><td><input class=mini id=positive_size_bonus step=any type=number><td>Positive Size Bonus'
+        + '<tr><td><input class=mini id=positive_size_max step=any type=number><td>Positive Size Max'
+        + '<tr><td><input class=mini id=time_limit step=any type=number><td>Time Limit</table>',
       'title': 'SpeedShape.htm',
       'ui': 'Score: <span id=score></span> | Time: <span id=time></span>',
     });
@@ -149,7 +149,7 @@ function repo_init(){
           core_ui_update({
             'ids': {
               'score': score,
-              'time': time_display + '/' + core_storage_data['time-limit'],
+              'time': time_display + '/' + core_storage_data.time_limit,
             },
           });
       },
